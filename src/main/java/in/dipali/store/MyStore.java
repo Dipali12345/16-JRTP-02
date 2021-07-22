9f776d86c4587c7022b0ba0471fa658e24ea8240
Dipali <dipalicthakre@gmail.com>
2021-07-22 10:23:39 +0530

    first commit

---
+++ src/main/java/in/dipali/store/MyStore.java
@@ -0,0 +1,11 @@
+package in.dipali.store;
+
+public class MyStore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I love my store");
+
+	}
+
+}
